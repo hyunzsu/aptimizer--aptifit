f6585230c628dec0cce19cda3683b5da81ca9c3e
aptimizer <aptimizer.co@gmail.com>
2024-07-19 14:40:29 +0900

    [🎨UI] `LoginPage` 생성
    `/`에서 결과지 확인 버튼을 클릭하면 로그인 버튼으로 넘어가게 된다.
    
    이곳에서 사용자는 자신의 고유 번호를 입력하고, 고유 번호가 데이터 베이스에 있다면 바로 `/result`로 이동하게 된다.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import s from "./LoginPage.module.css";
+import { LayoutContainer, Input, Button } from "@/components";
+
+const LoginPage = () => {
+  const [phone, setPhone] = useState("");
+
+  const handleInputChange = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleClick = () => {
+    console.log(phone);
+  };
+
+  return (
+    <main className={s.LoginPage}>
+      <LayoutContainer>
+        <h2 className={s.title}>로그인</h2>
+        <div className={s.inputContainer}>
+          <Input
+            label="고유 번호"
+            name="phone"
+            placeholder="고유 번호를 입력해주세요."
+            value={phone}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={s.buttonContainer}>
+          <Button onClick={handleClick}>다음</Button>
+        </div>
+      </LayoutContainer>
+    </main>
+  );
+};
+
+export default LoginPage;
