f0e3f3cd0f429aba8a3de46adab8ca414c0bb6b2
aptimizer <aptimizer.co@gmail.com>
2024-07-19 13:20:13 +0900

    [🎨UI] 버튼 위치 수정
    결과 확인 버튼이 추가됨에 따라 레이아웃 크기 조정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,25 @@
-import { LinkButton } from "@/components";
+"use client";
+
+import { LinkButton, Button } from "@/components";
 import { CircleAnimation } from "./_components";
 import s from "./HomePage.module.css";
 
 const HomePage = () => {
+  // 결과지로 바로 이동
+  const handleClick = () => {
+    console.log("click");
+  };
+
   return (
     <main className={s.HomePage}>
       {/* 1. 제목 */}
       <div className={s.titleContainer}>
         <h1 className={s.title}>앱티핏 대학 전공 적성 검사</h1>
         <p className={s.subtitle}>너의 전공을 찾아봐</p>
-        <LinkButton href="/information" label="시작하기" />
+        <div className={s.buttonContainer}>
+          <LinkButton href="/information" label="시작하기" />
+          <Button onClick={handleClick}>결과 확인</Button>
+        </div>
       </div>
 
       {/* 2. 애니메이션 */}
