57213b34c1fccb9835e4ca960f88859874c74cdd
aptimizer <aptimizer.co@gmail.com>
2024-07-19 19:23:03 +0900

    [🎨UI] 로딩 컴포넌트 UI 수정

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,10 +1,10 @@
 import s from "./Loading.module.css";
 
-const Loading = () => {
+const Loading = ({ text }) => {
   return (
     <div className={s.LoadingContainer}>
       <div className={s.spinner}></div>
-      <p className={s.text}>데이터를 로딩 중입니다...</p>
+      <p className={s.text}>{text}</p>
     </div>
   );
 };
