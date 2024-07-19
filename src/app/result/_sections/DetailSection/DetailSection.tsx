722049014781e946d492f17f7acf7746188bb1c1
aptimizer <aptimizer.co@gmail.com>
2024-07-19 20:42:27 +0900

    [🛠️수정] DetailSection 마크업 변경
    한라에서 사용되던 마크업 구조를 앱티핏에 맞춰 변경했다.

--- src/app/result/_sections/DetailSection/DetailSection.tsx
+++ src/app/result/_sections/DetailSection/DetailSection.tsx
@@ -38,15 +38,12 @@ const DetailSection = ({ resultData }) => {
         return (
           <div className={s.container}>
             <h3 className={s.subtitle}>가치</h3>
-            <div className={s.blur}>
-              <div className={s.blurInfoContainer}>해당 컨텐츠는 에듀 프리미엄에서 제공됩니다</div>
-            </div>
             <div>
               <div className={s.table}>
                 {/* 그래프 */}
                 <PentagonGraph result={result} />
                 {/* 테이블 */}
-                <Table result={result.strength} />
+                <Table result={result.value} />
               </div>
             </div>
             <div>
@@ -63,16 +60,13 @@ const DetailSection = ({ resultData }) => {
       case "개인특성":
         return (
           <div className={s.container}>
-            <div className={s.blur}>
-              <div className={s.blurInfoContainer}>해당 컨텐츠는 에듀 프리미엄에서 제공됩니다</div>
-            </div>
-            <h3 className={s.subtitle}>개인특성</h3>
+            <h3 className={s.subtitle}>가치</h3>
             <div>
               <div className={s.table}>
                 {/* 그래프 */}
                 <PentagonGraph result={result} />
                 {/* 테이블 */}
-                <Table result={result.strength} />
+                <Table result={result.value} />
               </div>
             </div>
             <div>
@@ -86,56 +80,25 @@ const DetailSection = ({ resultData }) => {
             </div>
           </div>
         );
-      case "흥미1":
+      case "흥미":
         return (
           <div className={s.container}>
-            <h3 className={s.subtitle}>흥미1</h3>
+            <h3 className={s.subtitle}>흥미</h3>
             <div>
               <div className={s.table}>
                 {/* 테이블 */}
-                <Image src={`data:image/png;base64,${wordcloud_image_base64}`} alt="" width={1000} height={350} />
-                <Table result={result.interests1} />
+                {/* <Image src={`data:image/png;base64,${wordcloud_image_base64}`} alt="" width={1000} height={350} /> */}
+                {/* <Table result={result.interests1} /> */}
               </div>
             </div>
             <div>
-              {result.interests1.descriptions.map((v, i) => {
+              {/* {result.interests1.descriptions.map((v, i) => {
                 return (
                   <p key={i} className={s.description}>
                     {v}
                   </p>
                 );
-              })}
-            </div>
-          </div>
-        );
-      case "흥미2":
-        return (
-          <div className={s.container}>
-            <h3 className={s.subtitle}>흥미2</h3>
-            <div className={s.keywordContainer}>
-              {result.interests2.details.map((v, i) => {
-                const { Interests_main, Interests_sub1, Interests_sub2, Interests_sub3, Interests_sub4 } = v;
-
-                return (
-                  <div key={i} className={s.interests2Container}>
-                    <div className={`${s.interests2Keyword} ${s.interests2KeywordMain}`}>{Interests_main}</div>
-                    <div className={s.interests2Keyword}>{Interests_sub1}</div>
-                    <div className={s.interests2Keyword}>{Interests_sub2}</div>
-                    <div className={s.interests2Keyword}>{Interests_sub3}</div>
-                    <div className={s.interests2Keyword}>{Interests_sub4}</div>
-                    <div className={s.line}></div>
-                  </div>
-                );
-              })}
-            </div>
-            <div>
-              {result.interests2.descriptions.map((v, i) => {
-                return (
-                  <p key={i} className={s.description}>
-                    {v}
-                  </p>
-                );
-              })}
+              })} */}
             </div>
           </div>
         );
