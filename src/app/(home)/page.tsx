68e6314886dd8f4afd30d7314187fadda9168e25
aptimizer <aptimizer.co@gmail.com>
2024-08-05 16:22:42 +0900

    [🎨UI] 메인페이지 제목 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,8 +7,7 @@ const HomePage = () => {
     <main className={s.HomePage}>
       {/* 1. 제목 */}
       <div className={s.titleContainer}>
-        <h1 className={s.title}>앱티핏 대학 전공 적성 검사</h1>
-        <p className={s.subtitle}>HL X 앱티마이저</p>
+        <h1 className={s.title}>에듀 프리미엄 체험판</h1>
         <div className={s.buttonContainer}>
           <LinkButton href="/login" label="시작하기" />
         </div>
