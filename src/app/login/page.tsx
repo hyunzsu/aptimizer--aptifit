49a437d361ee6055dc31d23d2441d618d09d515c
hyunzsu <hyunzsu@naver.com>
2024-08-28 14:53:22 +0900

    [🛡️타입] validatePhoneNumber 타입 지정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
   };
 
   // 휴대폰 형식 인증
-  const validatePhoneNumber = (phoneNumber) => {
+  const validatePhoneNumber = (phoneNumber: string): boolean => {
     const phoneRegex = /^(010|011|016|017|018|019)-?\d{3,4}-?\d{4}$/;
     return phoneRegex.test(phoneNumber);
   };
