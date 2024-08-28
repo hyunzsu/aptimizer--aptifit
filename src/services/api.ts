4a56a947c1d2bf751b2858589b08fe19f03570fe
hyunzsu <hyunzsu@naver.com>
2024-08-28 17:20:19 +0900

    [🛠️리팩토링] services 폴더 registerUser 통신 함수 추가

--- src/services/api.ts
+++ src/services/api.ts
@@ -21,3 +21,28 @@ export const checkUser = async (phone: string) => {
     throw error;
   }
 };
+
+// 회원가입: 사용자 정보 전송하는 함수
+export const registerUser = async (name: string, phone: string) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_INITIALIZE_HANLA}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, phone }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
