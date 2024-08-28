f554b1704df3dc11c708f8fcec417a22b85efa0c
hyunzsu <hyunzsu@naver.com>
2024-08-28 15:01:27 +0900

    [🛠️리팩토링] services 폴더 생성 및 checkUser 통신 함수 분리

---
+++ src/services/api.ts
@@ -0,0 +1,23 @@
+// 로그인: 회원 정보 확인하는 함수
+export const checkUser = async (phone) => {
+  const data = { phone };
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_LOGIN_HANLA}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
